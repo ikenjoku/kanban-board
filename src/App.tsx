8670d7e713f367ddd6c52a7504eabd6a707b0964
Ike Njoku <ikeenjoku@gmail.com>
2020-07-24 21:17:53 +0100

    scaffold app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { MainContainer } from './styles'
+import { Column } from './Column'
+import { Card } from './Card'
+import { AddNewItem } from './AddNewItem'
 
 function App() {
   return (
     <MainContainer>
-      Columns go here
+      <Column title="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column title="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column title="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem
+        toggleButtonText='+ Add another list'
+        onAdd={text => console.log(text)}
+      />
     </MainContainer>
   );
 }
