f0191894003798cd112386cb49346dce60ba4d56
Ike Njoku <ikeenjoku@gmail.com>
2020-07-26 12:13:14 +0100

    use context data

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { ColumnContainer, ColumnTitle } from './styles'
 import { AddNewItem } from './AddNewItem'
+import { Card } from './Card'
+import { useAppState } from './context'
 
 interface ColumnProps {
   title: string
+  index: number
 }
 
-export function Column ({ title, children }: React.PropsWithChildren<ColumnProps>) {
+export function Column ({ title, index }: React.PropsWithChildren<ColumnProps>) {
+  const { state } = useAppState()
   return (
     <ColumnContainer>
       <ColumnTitle>
         { title }
       </ColumnTitle>
-      {children}
+      {state.lists[index].tasks.map(task => (
+        <Card text={task.text} key={task.id} />
+      ))}
     <AddNewItem
       toggleButtonText='+ Add another task'
       onAdd={console.log}
